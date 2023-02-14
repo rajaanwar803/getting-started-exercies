582484af447d5c8357f7970af1c40e8cbecb121c
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-14 23:55:25 +0500

    7 exercise done

---
+++ 07_number-eight/index.ts
@@ -0,0 +1,11 @@
+let addition = 5 + 3;
+console.log(addition);
+
+let subtraction = 12 - 4;
+console.log(subtraction);
+
+let multiplication = 4 * 2;
+console.log(multiplication);
+
+let division = 24 / 3;
+console.log(division);
