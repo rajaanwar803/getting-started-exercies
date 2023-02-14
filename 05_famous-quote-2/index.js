74d3e2bcc2d66040818a714ea6384ffb469d7b0a
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-14 23:13:03 +0500

    5 exercise done

---
+++ 05_famous-quote-2/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let famous_person = "Nelson Mandela";
+let quote = "It always seems impossible until it's done.";
+let message = `${famous_person} once said, "${quote}"`;
+console.log(message);
---
+++ 05_famous-quote-2/index.ts
@@ -0,0 +1,4 @@
+let famous_person: string = "Nelson Mandela";
+let quote: string = "It always seems impossible until it's done.";
+let message: string = `${famous_person} once said, "${quote}"`;
+console.log(message);
