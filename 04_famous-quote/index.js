1ab05b46c3fa5d5f9ea41f251bd9f865bef031a2
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-14 22:59:13 +0500

    JS added

---
+++ 04_famous-quote/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let author = "Allama Iqbal";
+let quote = "Khudi ko kar buland itna ke har taqder se pehle Khuda bande se khud poochhe bata teri raza kya hai";
+console.log(`${author} once said, "${quote}"`);
