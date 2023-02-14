d50cd27a4a0542092e920601ec2444bbdc33bb05
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-14 23:39:13 +0500

    6 exercise done

---
+++ 06_stripping-names/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "\t\n Muhammad Anwar Subhani \n\t";
+console.log(`Name with whitespaces: "${name}"`);
+console.log(`Name after stripping whitespaces: "${name.trim()}"`);
