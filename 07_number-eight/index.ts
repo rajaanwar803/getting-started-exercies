ee7053e5c1dfa58928cbcf7026e1713177b4b787
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-15 00:01:04 +0500

    8 exercise done & changes in 7

--- 07_number-eight/index.ts
+++ 07_number-eight/index.ts
@@ -1,11 +1,11 @@
-let addition = 5 + 3;
+let addition: number = 5 + 3;
 console.log(addition);
 
-let subtraction = 12 - 4;
+let subtraction: number = 12 - 4;
 console.log(subtraction);
 
-let multiplication = 4 * 2;
+let multiplication: number = 4 * 2;
 console.log(multiplication);
 
-let division = 24 / 3;
+let division: number = 24 / 3;
 console.log(division);
