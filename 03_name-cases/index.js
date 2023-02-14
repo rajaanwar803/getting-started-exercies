0da049b70ae031b4b13abf5ded944a326eae1dff
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-14 16:08:10 +0500

    Q3 folder and files created

---
+++ 03_name-cases/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "aNwaR";
+console.log(`Lowercase: ${name.toLowerCase()}`);
+console.log(`Uppercase: ${name.toUpperCase()}`);
---
+++ 03_name-cases/index.ts
@@ -0,0 +1,6 @@
+let name: string = "aNwaR";
+
+console.log(`Lowercase: ${name.toLowerCase()}`)
+console.log(`Uppercase: ${name.toUpperCase()}`)
+
+export{}
