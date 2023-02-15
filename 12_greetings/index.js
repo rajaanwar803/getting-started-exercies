91ac5a9e958fcbbf5d5212d284b107b455f7a366
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-15 15:42:55 +0500

    12 exercise done

---
+++ 12_greetings/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let names = ["Umar", "Ahmed", "Hamza", "Zain"];
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hi ${names[i]}, how are you?`);
+}
---
+++ 12_greetings/index.ts
@@ -0,0 +1,9 @@
+let names: string[] = ["Umar", "Ahmed", "Hamza", "Zain"];
+
+for (let i = 0; i < names.length; i++) {
+    console.log(`Hi ${names[i]}, how are you?`);
+}
+
+
+
+export {}
