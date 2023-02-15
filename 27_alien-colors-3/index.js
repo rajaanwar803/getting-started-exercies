49837ca4d3a6bf66f79a781cc3cd901cd7cae9fb
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-15 21:53:14 +0500

    27 exercise done

---
+++ 27_alien-colors-3/index.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Version for green color
+let alienColor = "green";
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+}
+else if (alienColor == 'yellow') {
+    console.log("The player just earned 10 points.");
+}
+else if (alienColor == 'red') {
+    console.log("The player just earned 15 points.");
+}
+// Version for red color
+alienColor = "red";
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+}
+else if (alienColor == 'yellow') {
+    console.log("The player just earned 10 points.");
+}
+else if (alienColor == 'red') {
+    console.log("The player just earned 15 points.");
+}
+// Version for yellow color
+alienColor = "yellow";
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+}
+else if (alienColor == 'yellow') {
+    console.log("The player just earned 10 points.");
+}
+else if (alienColor == 'red') {
+    console.log("The player just earned 15 points.");
+}
---
+++ 27_alien-colors-3/index.ts
@@ -0,0 +1,35 @@
+// Version for green color
+let alienColor: string = "green";
+
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+} else if (alienColor == 'yellow') {
+    console.log("The player just earned 10 points.");
+} else if (alienColor == 'red') {
+    console.log("The player just earned 15 points.");
+}
+
+// Version for red color
+alienColor = "red";
+
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+} else if (alienColor == 'yellow') {
+    console.log("The player just earned 10 points.");
+} else if (alienColor == 'red') {
+    console.log("The player just earned 15 points.");
+}
+
+// Version for yellow color
+alienColor = "yellow";
+
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+} else if (alienColor == 'yellow') {
+    console.log("The player just earned 10 points.");
+} else if (alienColor == 'red') {
+    console.log("The player just earned 15 points.");
+}
+
+
+export {}
