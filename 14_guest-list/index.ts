7fc063d0b226ba7b2bf3ca92d1a65eb27001d7d3
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-15 16:00:26 +0500

    14 exercise done

---
+++ 14_guest-list/index.ts
@@ -0,0 +1,5 @@
+let dinnerGuests: string[] = ["Mohsin", "Mustafa", "Mohsin"];
+
+for (let i = 0; i < dinnerGuests.length; i++) {
+    console.log(`Dear ${dinnerGuests[i]}, I would like to invite you to dinner next week. It would be an honor to have you join me for a meal and some great conversation.`);
+}
