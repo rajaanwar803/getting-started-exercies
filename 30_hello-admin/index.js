593b62493bad2cd5580517aab03d79393e6e1a71
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-15 22:35:04 +0500

    30 exercise done

---
+++ 30_hello-admin/index.js
@@ -0,0 +1,27 @@
+"use strict";
+let userNames = [
+    "anwar",
+    "afzal",
+    "admin",
+    "umar",
+    "kamal",
+];
+// One way
+/*
+for (let i = 0; i < userNames.length; i++) {
+    if (userNames[i] == 'admin') {
+        console.log(`Hello ${userNames[i]}, would you like to see a status report?`);
+    } else {
+        console.log(`Hello ${userNames[i]}, thank you for logging in again`);
+    }
+}
+*/
+// Second way
+for (let userName of userNames) {
+    if (userName == 'admin') {
+        console.log(`Hello ${userName}, would you like to see a status report?`);
+    }
+    else {
+        console.log(`Hello ${userName}, thank you for logging in again`);
+    }
+}
