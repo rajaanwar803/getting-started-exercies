5d3847c45d91a5c3cabf7293dbcfa992094022e1
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-15 21:45:00 +0500

    26 exercise done

---
+++ 26_alien-colors-2/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Version that runs if block
+let alienColor = "green";
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
+// Version that runs else block
+alienColor = 'red';
+if (alienColor == 'green') {
+    console.log("The player just earned 5 points.");
+}
+else {
+    console.log("The player just earned 10 points.");
+}
