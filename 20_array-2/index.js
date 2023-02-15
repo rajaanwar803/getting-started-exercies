3a72f630383cb6e36cdd56a39411875732a01bfb
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-15 20:19:09 +0500

    20 exercise done

---
+++ 20_array-2/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let programmingLanguages = [
+    "JavaScript",
+    "Python",
+    "Ruby",
+    "Java",
+    "Go"
+];
+console.log(programmingLanguages);
---
+++ 20_array-2/index.ts
@@ -0,0 +1,9 @@
+let programmingLanguages: string[] = [
+    "JavaScript",
+    "Python",
+    "Ruby",
+    "Java",
+    "Go"
+];
+
+console.log(programmingLanguages);
