b6e0a69a71e1b15731d88638612fe373553f35ba
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-15 15:22:22 +0500

    10 exercise done

---
+++ 10_add-comments/index.js
@@ -0,0 +1,15 @@
+"use strict";
+// Name: Muhammad Anwar Subhani Date: 15/02/23
+Object.defineProperty(exports, "__esModule", { value: true });
+/* This program is removing the whitespaces from the start and end of the name,
+\t is used for extra spaces that are equivalent to one tab,
+\n is creating a new line and moving texting to the new line
+trim() is used to remove the whitespaces from the start and end of the name,
+*/
+let name = "\t\n Muhammad Anwar Subhani \n\t";
+console.log(`Name with whitespaces: "${name}"`);
+console.log(`Name after stripping whitespaces: "${name.trim()}"\n`);
+// 2nd program 
+// The variable is storing favorite number and then printing a message where favorited is revealed.
+let favoriteNumber = 6;
+console.log(`My favorite number is ${favoriteNumber}.`);
---
+++ 10_add-comments/index.ts
@@ -0,0 +1,23 @@
+// Name: Muhammad Anwar Subhani Date: 15/02/23
+
+/* This program is removing the whitespaces from the start and end of the name,
+\t is used for extra spaces that are equivalent to one tab,
+\n is creating a new line and moving texting to the new line
+trim() is used to remove the whitespaces from the start and end of the name,
+*/
+let name: string = "\t\n Muhammad Anwar Subhani \n\t";
+console.log(`Name with whitespaces: "${name}"`);
+console.log(`Name after stripping whitespaces: "${name.trim()}"\n`);
+
+
+// 2nd program 
+// The variable is storing favorite number and then printing a message where favorited is revealed.
+let favoriteNumber: number = 6;
+console.log(`My favorite number is ${favoriteNumber}.`);
+
+
+
+/* Don't know much about as I did not learn it yet, I only that it is removing some issues and we're using it for 
+temporary solution, I will learn more about it in future and then will update the comment.
+*/
+export {}
