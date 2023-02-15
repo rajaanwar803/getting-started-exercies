67f663b7654ea6843f4dc49fdd3abf227eadffaf
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-15 22:42:28 +0500

    31 exercise done

---
+++ 31_no-user/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let userNames = [];
+if (userNames.length > 0) {
+    for (let userName of userNames) {
+        if (userName == 'admin') {
+            console.log(`Hello ${userName}, would you like to see a status report?`);
+        }
+        else {
+            console.log(`Hello ${userName}, thank you for logging in again`);
+        }
+    }
+}
+else {
+    console.log("We need to find some users!");
+}
