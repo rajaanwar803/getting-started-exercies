9797f29d8a348196d319b1a013c469ad7b3936a7
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-15 21:15:43 +0500

    23 exercise done

---
+++ 23_conditional-test/index.ts
@@ -0,0 +1,46 @@
+let x = 10;
+let y = 5;
+
+// Test 1
+console.log("Is x > y? I predict True.");
+console.log(x > y);
+
+// Test 2
+console.log("Is x < y? I predict False.");
+console.log(x < y);
+
+// Test 3
+console.log("Is x == y? I predict False.");
+console.log(x == y);
+
+// Test 4
+console.log("Is x != y? I predict True.");
+console.log(x != y);
+
+// Test 5
+console.log("Is x >= y + 5? I predict True.");
+console.log(x >= y + 5);
+
+// Test 6
+console.log("Is x <= y - 5? I predict False.");
+console.log(x <= y - 5);
+
+let name1 = "John";
+let name2 = "Mary";
+
+// Test 7
+console.log("Is name1 == 'John'? I predict True.");
+console.log(name1 == 'John');
+
+// Test 8
+console.log("Is name2 != 'Mary'? I predict False.");
+console.log(name2 != 'Mary');
+
+// Test 9
+console.log("Is name1 != name2? I predict True.");
+console.log(name1 != name2);
+
+// Test 10
+console.log("Is name1.length == name2.length? I predict False.");
+console.log(name1.length == name2.length);
+
