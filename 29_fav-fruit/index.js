16a62a9809b099f808a0f01f9afa0bb58b310aa3
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-15 22:23:41 +0500

    29 exercise done

---
+++ 29_fav-fruit/index.js
@@ -0,0 +1,21 @@
+"use strict";
+let favFruits = [
+    "peach",
+    "strawberry",
+    "mango",
+];
+if (favFruits.includes('mango')) {
+    console.log("You really like mangoes!");
+}
+if (favFruits.includes('peach')) {
+    console.log("You really like peaches!");
+}
+if (favFruits.includes('apple')) {
+    console.log("You really like apples!");
+}
+if (favFruits.includes('strawberry')) {
+    console.log("You really like strawberries!");
+}
+if (favFruits.includes('banana')) {
+    console.log("You really like bananas!");
+}
