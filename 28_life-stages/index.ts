1f2c9fb12076cd0868bcd6622ba175f4caa7dd65
rajaanwar803 <rajaanwar803@gmail.com>
2023-02-15 22:08:28 +0500

    28 exercise done

---
+++ 28_life-stages/index.ts
@@ -0,0 +1,16 @@
+let age: number = 22;
+
+if (age < 2) {
+    console.log("The person is baby.");
+} else if (age >= 2 && age < 4) {
+    console.log("The person is toddler.");
+} else if (age >= 4 && age < 13) {
+    console.log("The person is kid.");
+} else if (age >= 13 && age < 20) {
+    console.log("The person is teenager.");
+} else if (age >= 20 && age < 65) {
+    console.log("The person is adult.");
+} else if (age >= 65) {
+    console.log("The person is elder.");
+} 
+
