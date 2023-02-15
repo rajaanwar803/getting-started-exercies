2532ce378d164e07ca6d06fe72cd96656270277d
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-15 20:23:52 +0500

    21 exercise done

---
+++ 21_ts-object/index.js
@@ -0,0 +1,13 @@
+"use strict";
+let person = {
+    firstName: "Muhammad Anwar",
+    lastName: "Subhani",
+    age: 22,
+    gender: "male",
+    occupation: "student",
+    address: {
+        city: "Mirpur",
+        province: "Azad Kashmir",
+    },
+};
+console.log(person);
---
+++ 21_ts-object/index.ts
@@ -0,0 +1,14 @@
+let person = {
+    firstName: "Muhammad Anwar",
+    lastName: "Subhani",
+    age: 22,
+    gender: "male",
+    occupation: "student",
+    address: {
+      city: "Mirpur",
+      province: "Azad Kashmir",
+    },
+  };
+  
+  console.log(person);
+  
