a9aeaeba76a3ed54f24ef06f93df91c199c524b1
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-16 17:51:47 +0500

    39 exercise done

---
+++ 39_city-names/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function cityCountry(city, country) {
+    return `${city}, ${country}`;
+}
+console.log(cityCountry("Islamabad", "Pakistan"));
+console.log(cityCountry("Oslo", "Norway"));
+console.log(cityCountry("London", "UK"));
---
+++ 39_city-names/index.ts
@@ -0,0 +1,8 @@
+function cityCountry(city:string, country:string):string {
+    return `${city}, ${country}`
+}
+
+
+console.log(cityCountry("Islamabad", "Pakistan"));
+console.log(cityCountry("Oslo", "Norway"));
+console.log(cityCountry("London", "UK"));
