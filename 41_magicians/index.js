2567f4ff026c4b69a52725585cf20383f5fffff3
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-16 21:51:24 +0500

    41 exercise done

---
+++ 41_magicians/index.js
@@ -0,0 +1,12 @@
+"use strict";
+function showMagicians(magicians) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+let magicians = [
+    "Anwar",
+    "Kamal",
+    "Afzal",
+];
+showMagicians(magicians);
---
+++ 41_magicians/index.ts
@@ -0,0 +1,13 @@
+function showMagicians(magicians:string[]) {
+    for (let magician of magicians) {
+        console.log(magician);
+    }
+}
+
+let magicians: string[] = [
+    "Anwar",
+    "Kamal",
+    "Afzal",
+];
+
+showMagicians(magicians);
