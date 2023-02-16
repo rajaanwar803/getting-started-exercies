da2714b7d5977388b490cb294c3cbe3f0d093207
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-16 17:45:09 +0500

    38 exercise done

---
+++ 38_cities/index.js
@@ -0,0 +1,7 @@
+"use strict";
+function describeCity(city, country = "Pakistan") {
+    console.log(`${city} is in ${country}.`);
+}
+describeCity("Islamabad");
+describeCity("Mirpur");
+describeCity("London", "UK");
---
+++ 38_cities/index.ts
@@ -0,0 +1,7 @@
+function describeCity(city:string, country:string = "Pakistan") {
+    console.log(`${city} is in ${country}.`);
+}
+
+describeCity("Islamabad");
+describeCity("Mirpur");
+describeCity("London", "UK");
