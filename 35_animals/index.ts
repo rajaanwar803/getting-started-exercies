1460b8da84c379ce0400246813cec185ea3fff95
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-16 17:20:04 +0500

    35 exercise done

---
+++ 35_animals/index.ts
@@ -0,0 +1,16 @@
+let animals: string[] = [
+    "dog",
+    "cat",
+    "parrot",  
+];
+
+for (let animal of animals) {
+    console.log(animal);
+}
+
+for (let animal of animals) {
+    console.log(`A ${animal} would make a great pet.`);
+}
+
+console.log("Any of these animals would make a great pet!");
+
