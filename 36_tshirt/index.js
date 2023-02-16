9b88ffa07cd1fea3add811533ed55e0e0cf58440
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-16 17:28:32 +0500

    36 exercise done

---
+++ 36_tshirt/index.js
@@ -0,0 +1,5 @@
+"use strict";
+function makeShirt(size, message) {
+    console.log(`The size of shirt is ${size} and the message printed on it is: "${message}"`);
+}
+makeShirt("S", "PIAIC");
---
+++ 36_tshirt/index.ts
@@ -0,0 +1,5 @@
+function makeShirt(size:string, message:string) {
+    console.log(`The size of shirt is ${size} and the message printed on it is: "${message}"`);
+}
+
+makeShirt("S", "PIAIC");
