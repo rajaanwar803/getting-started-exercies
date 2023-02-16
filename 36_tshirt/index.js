ee7f16b51bfe6c4d2cb8dde5b71504a5902cb08e
Muhammad Anwar Subhani <rajaanwar803@gmail.com>
2023-02-16 17:37:15 +0500

    36 exercise done

--- 36_tshirt/index.js
+++ 36_tshirt/index.js
@@ -1,5 +1,5 @@
 "use strict";
 function makeShirt(size, message) {
-    console.log(`The size of shirt is ${size} and the message printed on it is: "${message}"`);
+    console.log(`Making a ${size} shirt with the message: ${message}`);
 }
-makeShirt("S", "PIAIC");
+makeShirt("small", "I love TypeScript");
